28cf67936811ee9753a7de10ed49e28ac06fc313
ralphcoder <abhinashkumaryadavofficial@gmail.com>
2021-07-13 07:07:06 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
